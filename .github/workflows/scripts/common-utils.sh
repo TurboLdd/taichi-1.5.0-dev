2bb6b24b52644da71854fbbe8830207967996794
TurboLdd <115919827+TurboLdd@users.noreply.github.com>
2024-04-13 23:42:52 +0800

    initial commit

---
+++ .github/workflows/scripts/common-utils.sh
@@ -0,0 +1,282 @@
+#!/bin/bash
+
+set -x
+
+setup_python() {
+    for conda in .cache/build-cache/miniforge3 miniconda miniconda3 miniforge3; do
+        if [[ -d $HOME/$conda ]]; then
+            source $HOME/$conda/bin/activate
+            conda activate "$PY"
+            break
+        fi
+    done
+    python3 -m pip install -U pip
+    python3 -m pip uninstall taichi taichi-nightly -y
+}
+
+install_taichi_wheel() {
+    if [ ! -z "$AMDGPU_TEST" ]; then
+        sudo chmod 666 /dev/kfd
+        sudo chmod 666 /dev/dri/*
+    fi
+
+    if [ "$(uname -s):$(uname -m)" == "Darwin:arm64" ]; then
+        # No FORTRAN compiler is currently working reliably on M1 Macs
+        # We can't just pip install scipy, using conda instead
+        conda install -y scipy
+    fi
+
+    python3 -m pip install dist/*.whl
+    if [ -z "$GPU_TEST" ]; then
+        python3 -m pip install -r requirements_test.txt
+        python3 -m pip install "torch>1.12.0; python_version < '3.10'"
+        # Paddle's develop package doesn't support CI's MACOS machine at present
+        if [[ $OSTYPE == "linux-"* ]]; then
+            python3 -m pip install "paddlepaddle==2.3.0; python_version < '3.10'"
+        fi
+    else
+        ## Only GPU machine uses system python.
+        export PATH=$PATH:$HOME/.local/bin
+        # pip will skip packages if already installed
+        python3 -m pip install -r requirements_test.txt
+        # Import Paddle's develop GPU package will occur error `Illegal Instruction`.
+
+        # Log hardware info for the current CI-bot
+        # There's random CI failure caused by "import paddle" (Linux)
+        # Top suspect is an issue with MKL support for specific CPU
+        echo "CI-bot CPU info:"
+        if [[ $OSTYPE == "linux-"* ]]; then
+            lscpu | grep "Model name"
+        elif [[ $OSTYPE == "darwin"* ]]; then
+            sysctl -a | grep machdep.cpu
+        fi
+    fi
+}
+
+function setup-sccache-local {
+    if [ -z $SCCACHE_ROOT ]; then
+        echo "Skipping sccache setup since SCCACHE_ROOT is not set"
+        return
+    fi
+
+    mkdir -p $SCCACHE_ROOT/{bin,cache}
+
+    export SCCACHE_DIR=$SCCACHE_ROOT/cache
+    export SCCACHE_CACHE_SIZE="10G"
+    export SCCACHE_LOG=error
+    export SCCACHE_ERROR_LOG=$SCCACHE_ROOT/sccache_error.log
+
+    echo "SCCACHE_ROOT: $SCCACHE_ROOT"
+
+    if [ ! -x $SCCACHE_ROOT/bin/sccache ]; then
+        if [[ $OSTYPE == "linux-"* ]]; then
+            wget https://github.com/mozilla/sccache/releases/download/v0.2.15/sccache-v0.2.15-x86_64-unknown-linux-musl.tar.gz
+            tar -xzf sccache-v0.2.15-x86_64-unknown-linux-musl.tar.gz
+            mv sccache-v0.2.15-x86_64-unknown-linux-musl/* $SCCACHE_ROOT/bin
+        elif [[ $(uname -m) == "arm64" ]]; then
+            wget https://github.com/mozilla/sccache/releases/download/v0.2.15/sccache-v0.2.15-aarch64-apple-darwin.tar.gz
+            tar -xzf sccache-v0.2.15-aarch64-apple-darwin.tar.gz
+            mv sccache-v0.2.15-aarch64-apple-darwin/* $SCCACHE_ROOT/bin
+        else
+            wget https://github.com/mozilla/sccache/releases/download/v0.2.15/sccache-v0.2.15-x86_64-apple-darwin.tar.gz
+            tar -xzf sccache-v0.2.15-x86_64-apple-darwin.tar.gz
+            mv sccache-v0.2.15-x86_64-apple-darwin/* $SCCACHE_ROOT/bin
+        fi
+        chmod +x $SCCACHE_ROOT/bin/sccache
+    fi
+
+    export PATH=$SCCACHE_ROOT/bin:$PATH
+    export TAICHI_CMAKE_ARGS="$TAICHI_CMAKE_ARGS -DCMAKE_C_COMPILER_LAUNCHER=sccache -DCMAKE_CXX_COMPILER_LAUNCHER=sccache"
+}
+
+function clear-taichi-offline-cache {
+    rm -rf ~/build-cache/dot-cache/taichi  # Clear taichi offline cache
+    rm -rf ~/.cache/taichi  # Clear taichi offline cache
+}
+
+function prepare-build-cache {
+    export CACHE_HOME=${1:-$HOME/build-cache}
+    mkdir -p $CACHE_HOME/{dot-cache/pip,dot-gradle,git-cache,sccache/{bin,cache}}
+    chmod 0777 $CACHE_HOME/ $CACHE_HOME/* || true
+    pushd $CACHE_HOME/git-cache
+    if [ ! -d objects ]; then git init --bare; fi
+    popd
+
+    clear-taichi-offline-cache
+
+    if [ ! -z $GITHUB_ENV ]; then
+        # for bare metal run
+        export SCCACHE_ROOT=$CACHE_HOME/sccache
+        export GIT_ALTERNATE_OBJECT_DIRECTORIES=$CACHE_HOME/git-cache/objects
+        echo SCCACHE_ROOT=$SCCACHE_ROOT >> $GITHUB_ENV
+        echo GIT_ALTERNATE_OBJECT_DIRECTORIES=$GIT_ALTERNATE_OBJECT_DIRECTORIES >> $GITHUB_ENV
+        echo CACHE_HOME=$CACHE_HOME >> $GITHUB_ENV
+    else
+        # container run
+        true
+    fi
+}
+
+function fix-build-cache-permission {
+    if [ "$(uname -s)" = "Linux" ]; then
+        sudo -n chown -R $(id -u):$(id -g) $CACHE_HOME || true
+    fi
+}
+
+function ci-docker-run {
+    ARGS="$@"
+    SHOULD_RM="--rm"
+    while [[ $# > 0 ]]; do
+        case $1 in
+            -n | --name)
+                shift
+                CONTAINER_NAME="$1"
+                SHOULD_RM=""
+                break
+                ;;
+        esac
+        shift
+    done
+
+    if [ ! -z $CONTAINER_NAME ]; then
+        docker rm -f $CONTAINER_NAME
+    fi
+
+    TI_ENVS=""
+    for i in $(env | grep ^TI_); do
+        TI_ENVS="$TI_ENVS -e $i"
+    done
+
+    CACHE_HOME=${CACHE_HOME:-$HOME/build-cache}
+
+    docker run \
+        -i \
+        $SHOULD_RM \
+        --user dev \
+        -e PY \
+        -e PROJECT_NAME \
+        -e LLVM_VERSION \
+        -e TAICHI_CMAKE_ARGS \
+        -e IN_DOCKER=true \
+        -e TI_CI=1 \
+        $TI_ENVS \
+        -e SCCACHE_ROOT=/var/lib/sccache \
+        -e CACHE_HOME=/var/lib/cache-home \
+        -e GIT_ALTERNATE_OBJECT_DIRECTORIES=/var/lib/git-cache/objects \
+        -v $(readlink -f $CACHE_HOME):/var/lib/cache-home \
+        -v $(readlink -f $CACHE_HOME/sccache):/var/lib/sccache \
+        -v $(readlink -f $CACHE_HOME/git-cache):/var/lib/git-cache \
+        -v $(readlink -f $CACHE_HOME/dot-cache):/home/dev/.cache \
+        -v $(readlink -f $CACHE_HOME/dot-gradle):/home/dev/.gradle \
+        $CI_DOCKER_RUN_EXTRA_ARGS \
+        $ARGS
+}
+
+function ci-docker-run-gpu {
+    for i in {0..9}; do
+        if xset -display ":$i" -q >/dev/null 2>&1; then
+            break
+        fi
+    done
+
+    if [ $? -ne 0 ]; then
+        echo "No display!"
+        exit 1
+    fi
+
+    ci-docker-run \
+        --runtime=nvidia \
+        --gpus 'all,"capabilities=graphics,utility,display,video,compute"' \
+        -e DISPLAY=:$i \
+        -e GPU_BUILD=ON \
+        -e GPU_TEST=ON \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        $@
+}
+
+function ci-docker-run-amdgpu {
+    for i in {0..9}; do
+        if xset -display ":$i" -q >/dev/null 2>&1; then
+            break
+        fi
+    done
+
+    if [ $? -ne 0 ]; then
+        echo "No display!"
+        exit 1
+    fi
+
+    ci-docker-run \
+        --device=/dev/kfd \
+        --device=/dev/dri \
+        --device=/dev/vga_arbiter \
+        --group-add=video \
+        -e DISPLAY=:$i \
+        -e GPU_TEST=ON \
+        -e AMDGPU_TEST=ON \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        $@
+}
+
+function setup-android-ndk-env {
+    export ANDROID_NDK_ROOT=${ANDROID_NDK_ROOT:-/android-sdk/ndk-bundle}
+    export ANDROID_CMAKE_ARGS="-DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK_ROOT}/build/cmake/android.toolchain.cmake -DANDROID_NATIVE_API_LEVEL=29 -DANDROID_ABI=arm64-v8a"
+    export TAICHI_CMAKE_ARGS="$TAICHI_CMAKE_ARGS $ANDROID_CMAKE_ARGS"
+    export PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
+}
+
+function grab-android-bot {
+    if [ -z "$REDIS_HOST" ]; then
+        echo "REDIS_HOST is not set"
+        exit 1
+    fi
+
+    export BOT_LOCK_COOKIE=$(cat /proc/sys/kernel/random/uuid)
+    while true; do
+        for bot in $(redis-cli -h $REDIS_HOST --raw lrange android-bots 0 -1); do
+            export BOT_LOCK_KEY="android-bot-lock:$bot"
+            LOCKED=$(redis-cli -h $REDIS_HOST --raw setnx $BOT_LOCK_KEY $BOT_LOCK_COOKIE)
+            if [ $LOCKED -eq 1 ]; then
+                redis-cli -h $REDIS_HOST --raw expire android-bot-lock:$bot 300 > /dev/null
+                break
+            fi
+        done
+
+        if [ "$LOCKED" == "1" ]; then
+            export BOT=$bot
+            break
+        fi
+
+        echo "No bots available, retrying..."
+        sleep 1
+    done
+}
+
+function release-android-bot {
+    if [ $(redis-cli -h $REDIS_HOST --raw get $BOT_LOCK_KEY) == $BOT_LOCK_COOKIE ]; then
+        redis-cli -h $REDIS_HOST --raw del $BOT_LOCK_KEY
+    fi
+}
+
+function run-android-app {
+    APK=$1
+    ACTIVITY=$2
+    WAIT_TIME=${3:-5}
+    grab-android-bot
+    /android-sdk/platform-tools/adb connect $BOT
+    /android-sdk/platform-tools/adb devices
+    /android-sdk/platform-tools/adb install $APK
+    sleep 1
+    /android-sdk/platform-tools/adb logcat -c
+    /android-sdk/platform-tools/adb shell am start $ACTIVITY
+    sleep $WAIT_TIME
+    /android-sdk/platform-tools/adb logcat -d -v time 'CRASH:E *:F' | grep -v -- '----- beginning of ' | tee logcat.log
+
+    /android-sdk/platform-tools/adb shell am force-stop $(echo $ACTIVITY | sed 's#/.*$##g')
+    /android-sdk/platform-tools/adb disconnect
+    release-android-bot
+    if [ -s logcat.log ]; then
+        echo "!!!!!!!!!!!!!! Something is wrong !!!!!!!!!!!!!!"
+        exit 1
+    fi
+}
---
+++ .github/workflows/scripts/unix_test.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+set -ex
+
+. $(dirname $0)/common-utils.sh
+
+export PYTHONUNBUFFERED=1
+
+export TAICHI_AOT_FOLDER_PATH="taichi/tests"
+export TI_SKIP_VERSION_CHECK=ON
+export LD_LIBRARY_PATH=$PWD/build/:$LD_LIBRARY_PATH
+export TI_OFFLINE_CACHE_FILE_PATH=$PWD/.cache/taichi
+
+
+pip3 install -i https://pypi.taichi.graphics/simple/ taichi-nightly
+[[ "$IN_DOCKER" == "true" ]] && cd taichi
+python3 tests/generate_compat_test_modules.py
+python3 -m pip uninstall taichi-nightly -y
+
+setup_python
+
+install_taichi_wheel
+
+python3 tests/run_c_api_compat_test.py
+
+ti diagnose
+ti changelog
+echo "wanted archs: $TI_WANTED_ARCHS"
+
+
+if [ -z "$TI_SKIP_CPP_TESTS" ]; then
+    echo "Running cpp tests on platform:" "${PLATFORM}"
+    # Temporary hack before CI Pipeline Overhaul
+    if [[ "$(uname -s)" == "Linux" ]]; then
+        if nvidia-smi -L | grep "Tesla P4"; then
+            python3 tests/run_tests.py --cpp -vr2 -t4 -m "not sm70"
+        else
+            python3 tests/run_tests.py --cpp -vr2 -t4
+        fi
+    else
+        python3 tests/run_tests.py --cpp -vr2 -t4
+    fi
+fi
+
+
+if [ "$TI_RUN_RELEASE_TESTS" == "1" ]; then
+    python3 -m pip install PyYAML
+    git clone https://github.com/taichi-dev/taichi-release-tests
+    pushd taichi-release-tests
+    git checkout 20230130
+    mkdir -p repos/taichi/python/taichi
+    EXAMPLES=$(cat <<EOF | python3 | tail -n 1
+import taichi.examples
+print(taichi.examples.__path__[0])
+EOF
+)
+    ln -sf $EXAMPLES repos/taichi/python/taichi/examples
+    pushd repos
+    git clone --depth=1 https://github.com/taichi-dev/quantaichi
+    git clone --depth=1 https://github.com/taichi-dev/difftaichi
+    popd
+
+    pushd repos/difftaichi
+    pip install -r requirements.txt
+    popd
+
+    python3 run.py --log=DEBUG --runners 1 timelines
+    popd
+fi
+
+function run-it {
+    ARCH=$1
+    PARALLELISM=$2
+    KEYS=${3:-"not torch and not paddle"}
+
+    if [[ $TI_WANTED_ARCHS == *"$1"* ]]; then
+        python3 tests/run_tests.py -vr2 -t$PARALLELISM -k "$KEYS" -m "not run_in_serial" -a $ARCH
+        python3 tests/run_tests.py -vr2 -t1 -k "$KEYS" -m "run_in_serial" -a $ARCH
+    fi
+}
+
+if [ -z "$GPU_TEST" ]; then
+    if [[ $PLATFORM == *"m1"* ]]; then
+        run-it cpu 4
+        run-it vulkan 4
+        run-it metal 2
+
+        python3 tests/run_tests.py -vr2 -t1 -k "torch" -a "$TI_WANTED_ARCHS"
+    else
+        # Fail fast, give priority to the error-prone tests
+        if [[ $OSTYPE == "linux-"* ]]; then
+            python3 tests/run_tests.py -vr2 -t1 -k "paddle" -a "$TI_WANTED_ARCHS"
+        fi
+        python3 tests/run_tests.py -vr2 -t4 -k "not paddle" -a "$TI_WANTED_ARCHS"
+    fi
+elif [ ! -z "$AMDGPU_TEST" ]; then
+    run-it cpu    $(nproc)
+    run-it amdgpu 8
+else
+    run-it cuda   8
+    run-it cpu    $(nproc)
+    run-it vulkan 8
+    run-it opengl 4
+    run-it gles   4
+
+    python3 tests/run_tests.py -vr2 -t1 -k "torch" -a "$TI_WANTED_ARCHS"
+    # Paddle's paddle.fluid.core.Tensor._ptr() is only available on develop branch, and CUDA version on linux will get error `Illegal Instruction`
+
+    # FIXME: Running gles test separatelyfor now, add gles to TI_WANTED_ARCHS once running "-a vulkan,opengl,gles" is fixed
+    if [[ $TI_WANTED_ARCHS == *opengl* ]]; then
+      python3 tests/run_tests.py -vr2 -t1 -k "torch" -a gles
+    fi
+fi
